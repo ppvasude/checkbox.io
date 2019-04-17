ebb48bf03ed75771c72472230e68afc41693f3a7
ppvasude <44244649+ppvasude@users.noreply.github.com>
2019-04-17 17:35:28 -0400

    Update server.js

--- server-side/site/server.js
+++ server-side/site/server.js
@@ -1,4 +1,5 @@
-var express = require('express'),
+const request = require('request');
+var express = require('express'),    
         cors = require('cors'),
 	marqdown = require('./marqdown.js'),
 	//routes = require('./routes/designer.js'),
@@ -30,7 +31,20 @@ app.post('/api/design/survey',
 	{
 		console.log(req.body.markdown);
 		//var text = marqdown.render( req.query.markdown );
-		var text = marqdown.render( req.body.markdown );
+	request.post('http://a751bd4b1613311e9a1cd0692d893a7e-811463482.us-east-2.elb.amazonaws.com:3002/api/design/survey',{
+	json: {
+		markdown: req.body.markdown
+	}
+	},
+		     (error,res,body) => {
+		if(error){
+			console.error(error);
+			return;
+		}
+		console.log("Response Body::"+body);
+		console.log("Response::"+res);
+	})
+		//var text = marqdown.render( req.body.markdown );
 		res.send( {preview: text} );
 	}
 );
