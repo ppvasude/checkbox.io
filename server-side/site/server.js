f7d98bf209ec0726d2a6e939d6318a3e6ad95050
ppvasude <44244649+ppvasude@users.noreply.github.com>
2019-04-17 19:11:20 -0400

    Update server.js

--- server-side/site/server.js
+++ server-side/site/server.js
@@ -32,7 +32,7 @@ app.post('/api/design/survey',
 	{
 		//console.log("RequestBodyMarkdown::"+req.body.markdown);
 		//var text = marqdown.render( req.query.markdown );
-	
+		var ansText;
 		request.post('http://a751bd4b1613311e9a1cd0692d893a7e-811463482.us-east-2.elb.amazonaws.com:3002/api/design/survey',{
 	json: {
 		markdown: req.body.markdown
@@ -45,12 +45,13 @@ app.post('/api/design/survey',
 		}
 		console.log("ResponseBodyMicroService::"+body.preview);
 		console.log("Response::"+res.json());
-			var ansText = body.preview;
-			res.send( {preview: ansText} );
+			ansText = body.preview;
+			//res.send( {preview: ansText} );
 	});
 	setTimeout(function() {
    var text = marqdown.render( req.body.markdown );
 		console.log("ResponseSelf::"+text);
+		res.send( {preview: ansText} );
 }, 500);
 		
 		//res.send( {preview: text} );
