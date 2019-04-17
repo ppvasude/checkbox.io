60d705d362653a13d0d5097806aae35a83a555e1
ppvasude <44244649+ppvasude@users.noreply.github.com>
2019-04-17 18:18:59 -0400

    Update server.js

--- server-side/site/server.js
+++ server-side/site/server.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const { parse } = require('querystring');
 var express = require('express'),    
         cors = require('cors'),
 	marqdown = require('./marqdown.js'),
@@ -29,7 +30,7 @@ app.options('/api/study/vote/submit/', cors(corsOptions));
 app.post('/api/design/survey', 
 	function(req,res)
 	{
-		console.log("RequestBodyMarkdown::"+req.body.markdown);
+		//console.log("RequestBodyMarkdown::"+req.body.markdown);
 		//var text = marqdown.render( req.query.markdown );
 		request.post('http://a751bd4b1613311e9a1cd0692d893a7e-811463482.us-east-2.elb.amazonaws.com:3002/api/design/survey',{
 	json: {
@@ -41,7 +42,7 @@ app.post('/api/design/survey',
 			console.error(error);
 			return;
 		}
-		console.log("Response Body::"+body);
+		console.log("Response Body::"+parse(body));
 		console.log("Response::"+res.json());
 	});
 		//var text = marqdown.render( req.body.markdown );
